525085dde90526f447159e66330dd40c13a12d95
Otabek Tuychiev <otawflash@gmail.com>
2022-07-04 05:55:46 +0900

    Initial Commit

---
+++ MasterClass_01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MasterClass_01
+//
+//  Created by Otabek Tuychiev on 04/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MasterClass_01/MasterClass_01App.swift
@@ -0,0 +1,17 @@
+//
+//  MasterClass_01App.swift
+//  MasterClass_01
+//
+//  Created by Otabek Tuychiev on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MasterClass_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
